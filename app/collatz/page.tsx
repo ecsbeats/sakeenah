e845627306cb8615ec766d3ad9fc95464d15bc82
ecsbeats <adamb@csxlabs.org>
2024-09-03 22:15:30 -0700

    fix input for seed number

--- app/collatz/page.tsx
+++ app/collatz/page.tsx
@@ -1,18 +1,23 @@
-"use client"
+import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
 import { MathJax, MathJaxContext } from "better-react-mathjax"
+import { Icon } from "@iconify/react";
 
 export default function Collatz() {
     return (
         <main className="flex flex-col gap-4">
-            <Card>
+            <Card className="max-w-96">
                 <CardHeader>
                     <CardTitle>Collatz Demo</CardTitle>
                     <CardDescription>September 3rd, 2024</CardDescription>
                 </CardHeader>
                 <CardContent>
-                        <p>This demo showcases a modified version of the collatz conjecture with new rules.</p>
-                        <p>Will put in a longer description later.</p>
+                        <p>This demo showcases a modified version of the collatz conjecture from Sakeenah Blumenfeld with new rules.</p>
+                        <div className="flex w-full items-center my-4 space-x-2">
+                            <Input type="number" placeholder="Seed number" />
+                            <Button type="submit"><Icon icon="mdi:arrow-right" className="text-xl" /></Button>
+                        </div>
                 </CardContent>
             </Card>
         </main>
